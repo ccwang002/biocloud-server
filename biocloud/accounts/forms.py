71c8371230356046a5d4011e141f34f1b8b64fa6
Liang Bo Wang <ccwang002@gmail.com>
2015-08-30 21:47:32 +0800

    fix form error not shown

--- biocloud/accounts/forms.py
+++ biocloud/accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from crispy_forms.helper import FormHelper
 from django.contrib.auth.forms import UserCreationForm as _UserCreationForm
 
 from django.contrib.auth.models import User
@@ -10,9 +11,19 @@ class AccountForm(forms.ModelForm):
         model = Account
         fields = ('description', )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.disable_csrf = True
 
 class UserCreationForm(_UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.disable_csrf = True
